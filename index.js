91b38ba4cc3d8e4b90486fe03c7f0b5732c50b80
krzysdz <krzysdz@users.noreply.github.com>
2021-03-23 22:06:01 +0100

    Add !create command

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ client.on("message", message => {
 	if (message.content === "!ping") {
 		message.channel.send("Pong.");
 	}
+	if (message.content === "!create") {
+		let name = message.author.username;
+		message.guild?.channels.create(name, {
+			type: "text", //This create a text channel, you can make a voice one too, by changing "text" to "voice"
+			permissionOverwrites: [
+				{
+					id: message.guild.roles.everyone, //To make it be seen by a certain role, use an ID instead
+					allow: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"], //Allow permissions
+					deny: ["VIEW_CHANNEL", "SEND_MESSAGES", "READ_MESSAGE_HISTORY"] //Deny permissions
+				}
+			],
+		})
+			.then(console.log)
+			.catch(console.error);
+	}
 });
 
 client.login(loginToken);
